741a9aec051ceec26bae5515b5aff7bf615206de
guohaiqiang <837557495@qq.com>
2017-12-25 15:06:56 +0800

    qq在线聊天

--- app/home/home.ctrl.js
+++ app/home/home.ctrl.js
@@ -6,16 +6,22 @@ define([
 ], function (app, config, constants, layer) {
     app.registerController('homeCtrl', ['$scope', '$state', '$rootScope', 'localStorageService', '$$neptune', '$timeout',
         function ($scope, $state, $rootScope, localStorageService, $$neptune, $timeout) {
-
-
-            var init = function () {
-                $("#lazyload_3").slide({ trigger: "click", effect: "fade",auto: true,duration: 50, lazyload: true });
+            //打开qq聊天
+            $scope.qqChat = function () {
+                window.open('http://wpa.qq.com/msgrd?v=3&uin=1016075562&site=qq&menu=yes','_brank');
+            };
+            //轮播图
+            var carouselFigure =function () {
+                $("#lazyload_3").slide({ trigger: "click", effect: "fade",auto: true,duration: 5, lazyload: true });
                 $("pre.jsCode").snippet("javascript", {style: "custom_js", showNum: false});
                 $("pre.jsCodeNum").snippet("javascript", {style: "custom_js"});
+            };
+            var init = function () {
+                carouselFigure()//初始化加载轮播图
 
-                $scope.nihao = function () {
-                    window.open("tencent://message/?uin=2546150289")
-                }
+
+
+
  //                $('.rm-nav').rMenu({
  //                    minWidth: '960px'
  //                });
