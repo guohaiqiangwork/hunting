9a176761e81209f4942ae374a039802fe594e269
AGOE21-08221219\Administrator <1979202959@.com>
2018-01-03 16:05:37 +0800

    关于我们

--- app/aboutUs/aboutUs.ctrl.js
+++ app/aboutUs/aboutUs.ctrl.js
@@ -8,6 +8,14 @@ define([
     app.registerController('aboutUsCtrl', ['$scope', '$state', '$rootScope', '$$neptune', '$timeout', '$stateParams',
         function ($scope, $state, $rootScope, $$neptune, $timeout, $stateParams) {
             $scope.leftTitle = $stateParams.id;
+            $scope.active = $scope.leftTitle;
+            /**
+             * 界面跳转
+             * @param info
+             */
+            $scope.switchView = function (info) {
+                $scope.active = info;
+            };
             var init = function () {
                 console.log('关于我们');
                 new WOW({
