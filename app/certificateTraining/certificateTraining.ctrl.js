dd6f5756a646ee539a54603acd14e4c9fb2f2fe5
guohaiqiang <837557495@qq.com>
2018-01-04 11:25:56 +0800

    证书培训

--- app/certificateTraining/certificateTraining.ctrl.js
+++ app/certificateTraining/certificateTraining.ctrl.js
@@ -6,11 +6,20 @@ define([
 ], function (app, config, constants, layer) {
     app.registerController('certificateTrainingCtrl', ['$scope', '$state', '$rootScope', '$$neptune', '$timeout', '$stateParams',
         function ($scope, $state, $rootScope, $$neptune, $timeout, $stateParams) {
-            $scope.leftTitle = $stateParams.id;
+            $scope.leftTitle = $stateParams.id || '证书培训';
+            $rootScope.active = $scope.leftTitle;
             var init = function () {
-                console.log('证书培训')
+
             };
 
+
+            /**
+             * 界面跳转
+             * @param info
+             */
+            $scope.switchView = function (info) {
+                $rootScope.active = info;
+            };
             init();
         }]);
 
