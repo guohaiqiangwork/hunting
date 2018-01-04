4f6e4dd74d1d7a7f4d13358e9cd9330aa90676e3
guohaiqiang <837557495@qq.com>
2018-01-04 10:51:51 +0800

    资质动态

--- app/common/main.ctrl.js
+++ app/common/main.ctrl.js
@@ -60,6 +60,7 @@ define([
              */
             $scope.navClick = function (activeIndex,navLeftIndex) {
                 $scope.activeIndex = activeIndex;
+                console.log($scope.activeIndex)
                 switch (activeIndex){
                     case 0: // 首页
                         $state.go('home');
