e4e79c26993b2226fb76f97ec6485ac4ae15bb69
lijialong\Administrator <1979202959@.com>
2018-01-04 16:59:52 +0800

    查询中心

--- app/queryCenter/queryCenter.ctrl.js
+++ app/queryCenter/queryCenter.ctrl.js
@@ -6,6 +6,17 @@ define([
 ], function (app, config, constants, layer) {
     app.registerController('queryCenterCtrl', ['$scope', '$state', '$rootScope', 'localStorageService', '$$neptune', '$timeout',
         function ($scope, $state, $rootScope, localStorageService, $$neptune, $timeout) {
+            $scope.temp = {};
+            $scope.goto = function (number) {
+                for (var k in $scope.temp) {
+                    if (number !== k) {
+                        $scope.temp[k] = false;
+                    }
+                }
+                console.log("aaa");
+                $scope.temp[number] = !$scope.temp[number];
+            };
+
             var init = function () {
 
             };
