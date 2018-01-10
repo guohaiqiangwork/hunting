bcaa6a91f28474d43fbd88ad23a53052d5e6c347
lijialong\Administrator <1979202959@.com>
2018-01-10 17:10:05 +0800

    修改

--- app/individualCallings/individualCallings.ctrl.js
+++ app/individualCallings/individualCallings.ctrl.js
@@ -6,6 +6,14 @@ define([
 ], function (app, config, constants, layer) {
     app.registerController('individualCallingsCtrl', ['$scope', '$state', '$rootScope', 'localStorageService', '$$neptune', '$timeout',
         function ($scope, $state, $rootScope, localStorageService, $$neptune, $timeout) {
+            $scope.active = '个人挂证';
+            /**
+             * 界面跳转
+             * @param info
+             */
+            $scope.switchView = function (info) {
+                $scope.active = info;
+            };
 
             var init = function () {
                 console.log("企业寻证");
